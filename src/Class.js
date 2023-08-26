c6a961f1b3971a2af5bfe1a8ea868dc333e7cf6f
Ayush Manware <ayushmanware19@gmail.com>
2023-08-26 13:18:16 +0530

    UI

--- src/Class.js
+++ src/Class.js
@@ -9,7 +9,7 @@ class Xyz extends React.Component{
     render(){
         return(
             <>
-            <div className="container1 containerSame">
+            <div className="container2 containerSame">
            <h2>This is Created Using Class Component</h2>
            <p>This is done using external CSS</p>
            <p>This is done using Internal CSS</p>
